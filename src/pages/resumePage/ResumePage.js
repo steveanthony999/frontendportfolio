cdf61ad90f0f5feae5b9312b66c5b1b076e4e199
Steven Woodward <steveanthony999@gmail.com>
2022-03-11 01:58:23 -0800

    changed to public folder

--- src/pages/resumePage/ResumePage.js
+++ src/pages/resumePage/ResumePage.js
@@ -1,9 +1,13 @@
-import Resume from '../../assets/StevenWoodwardResume.pdf';
+// import Resume from '../../assets/StevenWoodwardResume.pdf';
 
 const ResumePage = () => {
   return (
-    <div className='ResumePage'>
-      <iframe src={Resume} width='100%' height='2000px'></iframe>
+    <div>
+      <iframe
+        src={`/StevenWoodwardResume.pdf`}
+        width='100%'
+        height='2000px'
+      ></iframe>
     </div>
   );
 };
